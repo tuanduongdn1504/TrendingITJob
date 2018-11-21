1e5a3fe50e96420ea2a772381d2a5e03dce24515
Duong Tuan <tuan.duong@enouvo.com>
2018-11-21 13:13:38 +0700

    no message

--- knexfile.js
+++ knexfile.js
@@ -27,7 +27,8 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection:
+      'postgres://wbrtvnuzvkzugi:9614a0d1a994a22a979dc330b130d682712fb0ccdd95761e10cffc896a606561@ec2-54-225-110-156.compute-1.amazonaws.com:5432/dak81h5ppbblvi',
     migrations: {
       directory: path.join(BASE_PATH, 'migrations')
     },
@@ -36,4 +37,3 @@ module.exports = {
     }
   }
 };
-
