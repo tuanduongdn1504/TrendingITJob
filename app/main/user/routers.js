dfe4da520177690f81ffe5983e4493dd404af512
Duong Tuan <tuan.duong@enouvo.com>
2018-11-21 11:05:54 +0700

    no message

--- app/main/user/routers.js
+++ app/main/user/routers.js
@@ -5,37 +5,37 @@ const handler = require('./handler');
 const Routes = [
   {
     method: 'GET',
-    path: '/api/v1/users',
+    path: 'api/v1/users',
     config: handler.getMany
   },
   {
     method: 'GET',
-    path: '/api/v1/users/{id}',
+    path: 'api/v1/users/{id}',
     config: handler.getOne
   },
   {
     method: 'GET',
-    path: '/api/v1/users/{id}/resetPassword',
+    path: 'api/v1/users/{id}/resetPassword',
     config: handler.resetPassword
   },
   {
     method: 'POST',
-    path: '/api/v1/users/{id}/updatePassword',
+    path: 'api/v1/users/{id}/updatePassword',
     config: handler.updatePassword
   },
   {
     method: 'POST',
-    path: '/api/v1/users',
+    path: 'api/v1/users',
     config: handler.createOne
   },
   {
     method: 'PUT',
-    path: '/api/v1/users/{id}',
+    path: 'api/v1/users/{id}',
     config: handler.updateOne
   },
   {
     method: 'DELETE',
-    path: '/api/v1/users/{id}',
+    path: 'api/v1/users/{id}',
     config: handler.deleteOne
   }
 ];
