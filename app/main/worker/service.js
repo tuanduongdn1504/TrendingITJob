35cad6e9f5f0dbeedd2ffb99876724e23bec14f5
Duong Tuan <tuan.duong@enouvo.com>
2018-12-10 17:47:53 +0700

    UPDATE final workerModal

--- app/main/worker/service.js
+++ app/main/worker/service.js
@@ -1,14 +1,42 @@
 'use strict';
 
 const Boom = require('boom');
+const moment = require('moment');
 const Models = require('../../db/models');
+const bcrypt = require('bcrypt');
+const { ROLES, ROLENAMES } = require('../../constants/roles');
+
+exports.getSummary = async () => {
+  const result = await Promise.all([
+    getTotalActiveWorker(),
+    getTotalWorked(),
+    getTotalWorkedByTime('day'),
+    getTotalWorkedByTime('month'),
+    getTotalWorkedByTime('year')
+  ]);
+  return {
+    numberOfTotalActiveWorker: result[0],
+    numberOfTotalWorkedWorker: result[1],
+    numberOfTotalWorkedByDay: result[2],
+    numberOfTotalWorkedByMonth: result[3],
+    numberOfTotalWorkedByYear: result[4]
+  };
+};
+
+exports.getWorkerBooking = async (userId, query) => {
+  return Models.Booking.queryBuilder(query).where({
+    userId
+  });
+};
 
 exports.getAllWorker = async (query) => {
   return Models.Worker.queryBuilder(query);
 };
 
 exports.getOneWorker = async (id) => {
-  const result = await Models.Worker.query().findById(id);
+  const result = await Models.Worker.query()
+    .findById(id)
+    .eager('[users]');
   if (!result) {
     throw Boom.notFound('Worker not found');
   }
@@ -17,11 +45,62 @@ exports.getOneWorker = async (id) => {
 };
 
 exports.createWorker = async (body) => {
+  body.isActive = true;
   return Models.Worker.query().insert(body);
+};
+// exports.createService = async (body) => {
+//   const servicePackage = await Models.ServicePackage.query().findById(
+//     body.packageId
+//   );
+//   body.package = servicePackage;
+//   if (!body.totalAmount) {
+//     body.totalAmount = servicePackage.price * body.quantity;
+//   }
+//   const result = await Models.Service.query()
+//     .insert(body)
+//     .returning('*');
+//   if (body.isPaid) {
+//     await Models.Payment.query().insert({
+//       amount: body.totalAmount,
+//       description: `Pay for service id#${result.id}`,
+//       paymentTypeId: servicePackage.paymentTypeId,
+//       transactionType: 'INCOME',
+//       bookingId: body.bookingId
+//     });
+//   }
+//   return result;
+// };
+
+exports.activeWorker = async (id, body) => {
+  try {
+    const updatePayload = { isActive: true };
+    const result = await Models.Worker.query().patchAndFetchById(
+      id,
+      updatePayload
+    );
+    return result;
+  } catch (err) {
+    throw err;
+  }
+};
+exports.inactiveWorker = async (id, body) => {
+  try {
+    const updatePayload = { isActive: false };
+    const result = await Models.Worker.query().patchAndFetchById(
+      id,
+      updatePayload
+    );
+    return result;
+  } catch (err) {
+    throw err;
+  }
 };
 
 exports.updateWorker = async (id, body) => {
   try {
+    if (body.password) {
+      body.password = await bcrypt.hash(body.password, 5);
+    }
     const result = await Models.Worker.query()
       .update(body)
       .where('id', id)
@@ -60,3 +139,32 @@ exports.deleteWorker = async (id) => {
     throw err;
   }
 };
+
+async function getTotalActiveWorker() {
+  const result = await await Models.Worker.query().count('isActive');
+  return result[0].count;
+}
+
+async function getTotalWorked() {
+  const result = await await Models.Booking.query()
+    .countDistinct('userId')
+    .whereIn('status', ['PENDING', 'INPROGRESS']);
+  return result[0].count;
+}
+
+async function getTotalWorkedByTime(time) {
+  const result = await await Models.Booking.query()
+    .countDistinct('userId')
+    .whereBetween('startTime', [
+      moment
+        .utc()
+        .startOf(time)
+        .toISOString(),
+      moment
+        .utc()
+        .endOf(time)
+        .toISOString()
+    ])
+    .whereNot('status', 'CANCELLED');
+  return result[0].count;
+}
