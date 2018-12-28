b0eea34a4bbb20730146de3d8dd99cc7db419021
Duong Tuan <tuan.duong@enouvo.com>
2018-12-28 17:05:16 +0700

    Export Tag Modal

--- app/db/models/index.js
+++ app/db/models/index.js
@@ -4,10 +4,12 @@ const User = require('./User');
 const Worker = require('./Worker');
 const Company = require('./Company');
 const ProductOwner = require('./ProductOwner');
+const Tag = require('./Tag');
 
 module.exports = {
   User,
   Worker,
   Company,
-  ProductOwner
+  ProductOwner,
+  Tag
 };
