13b1c41e73583ccda29636d2f4d943b6711b9b7a
Duong Tuan <tuan.duong@enouvo.com>
2019-01-03 15:05:09 +0700

    EXPORTS tagPost Model

--- app/db/models/index.js
+++ app/db/models/index.js
@@ -8,6 +8,7 @@ const Tag = require('./Tag');
 const Cv = require('./Cv');
 const TagCv = require('./TagCv');
 const Post = require('./Post');
+const TagPost = require('./TagPost');
 
 module.exports = {
   User,
@@ -17,5 +18,6 @@ module.exports = {
   Tag,
   Cv,
   TagCv,
-  Post
+  Post,
+  TagPost
 };
